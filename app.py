341d37735718bc1f5efcd1dbecb6f10cefbdd169
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-07-30 12:25:12 +0530

    add jupyter notebook

--- app.py
+++
@@ -1,9 +0,0 @@
-import requests
-import logging
-import os
-from bs4 import BeautifulSoup as bs  # For scrapping
-from urllib.request import urlopen   # Requesting server
-
-save_dir = "images/"
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import requests
+import logging
+import os
+from bs4 import BeautifulSoup as bs  # For scrapping
+from urllib.request import urlopen   # Requesting server
+
+application = Flask('__main__')
+app = application
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+
+if '__main__' == '__name__':
+    app.run()
